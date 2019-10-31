fbdc4572fe06bf22541ee73c1e211ddd82c46692
binuputhuchery <binuputhuchery@gmail.com>
2019-10-31 23:19:00 +0530

    Included react material for ui elements, updated sidebar component

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
 function Sidebar() {
     return (
+        <aside>
         <h2>Sidebar</h2>
+            <Box color="text.primary" clone>
+                <Button color="primary">
+                    My Account
+                </Button>
+            </Box>
+        </aside>
     )
 }
 
